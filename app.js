17cab50cfb6c6047cd795df5dfb7746c7084a0d1
Oscar Hackman <ohackman1@gmail.com>
2020-06-22 22:15:54 +0300

    todo routes finished

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ connectDB();
 
 require('./passport');
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(
     cookieSession({
--- routes/todos.js
+++ routes/todos.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const requireLogin = require('../middlewares/requireLogin');
 const { check, validationResult } = require('express-validator');
@@ -26,11 +27,11 @@ router.get('/', requireLogin, async (req,res) => {
 // @description add a todos
 // @access private
 
-//[requireLogin,[
-//    check('title', 'Title is Required').not().isEmpty()
-//]],
 
-router.post('/', async (req,res) => {
+
+router.post('/', [requireLogin,[
+   check('title', 'Title is Required').not().isEmpty()
+]], async (req,res) => {
     const errors = validationResult(req);
     //returns array of errors
     if(!errors.isEmpty()){
@@ -56,7 +57,59 @@ router.post('/', async (req,res) => {
 
 })
 
+// update a todo
 
+router.put('/:id', requireLogin , async(req, res) => {
+    const { title, description, time } = req.body;
+    const todoField = [];
+    if(title) todoField.title = title;
+    if(description) todoField.description = description;
+    if(time) todoField.time = time;
+
+    try {
+        let todo = await Todo.findById(req.params.id);
+        if(!todo){
+            return res.status(404).json({ msg: 'Todo not found'});
+        } 
+        // checs if user owns choosen data
+        if(todo.user.toString() !== req.user.id){
+            return res.status(401).json({ msg: 'Not Auhtorized'});
+        }
+        // sets data to 'todoField' and updates, if not existing creates new
+        todo = await Todo.findByIdAndUpdate(req.params.id,
+            { $set: todoField },
+            { new: true });
+
+        res.json(todo);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
+})
+
+// delete a todo
+
+router.delete('/:id', requireLogin, async(req, res) => {
+    try {
+        let todo = await Todo.findById(req.params.id);
+        if(!todo){
+            return res.status(404).json({ msg: 'Todo not found'});
+        } 
+        // checs if user owns choosen data
+        if(todo.user.toString() !== req.user.id){
+            return res.status(401).json({ msg: 'Not Auhtorized'});
+        }
+        // finds and deletes the 'todo' with correct id
+        await Todo.findByIdAndRemove(req.params.id);
+
+        res.json({msg: 'Contact removed'});
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
 
 
 module.exports = router;
