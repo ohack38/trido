f74d5b7ce7d1053a49f383833dda56682850f274
Oscar Hackman <ohackman1@gmail.com>
2020-06-30 22:15:31 +0300

    Dashboard and fe routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
+const App = () =>{
   return (
+    <Router>
       <Fragment>
         <div className='container'>
-             
+             <Switch>
+               <Route exact path='/' component={Dashboard}/>
+             </Switch>
         </div>
       </Fragment>
+    </Router>
+      
     
   );
 }
